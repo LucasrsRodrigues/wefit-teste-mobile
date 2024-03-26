e432b454e5bf820d7dbb4433560febd1c5498126
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-03-26 11:55:13 -0300

    feat: add global theme

---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    background: "#F6F6F5",
+    card: "#FFFFFF",
+    primary: "#FFD02C",
+    primary_light: "#FAF3DC",
+    heading: "#070707",
+    paragraph: "#9A9A9A"
+  },
+  fonts: {
+    primary: {
+      regular: "",
+      bold: ""
+    },
+    secondary: {
+      regular: "",
+      medium: ""
+    }
+  },
+}
