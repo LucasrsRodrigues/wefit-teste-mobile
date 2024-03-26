2cc336a6e11e211d238f91a25ffa3660844548a0
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-03-26 12:31:15 -0300

    feat: add SplashScreen

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Logo from "@assets/logo.svg";
 
 import * as S from './styles';
 
 export function SplashScreen() {
   return (
     <S.Container>
-
+      <Logo />
     </S.Container>
   );
 }
