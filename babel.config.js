b99911bb0b27cd04394f01b8ef59d9747068ee51
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-03-26 11:58:28 -0300

    feat: add relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@global': './src/global',
+            '@infrastructure': './src/infrastructure',
+            '@backend': './src/backend'
+          }
+        }
+      ],
+    ]
   };
 };
